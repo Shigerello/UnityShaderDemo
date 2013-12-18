88318515bc4f2436cc5ca485f103d880ec4e1832
Shigerello <shigerello@gmail.com>
2013-12-18 12:22:35 +0900

    シェーダのデモ用に3Dモデル・マテリアル・シーンなどを追加。

---
+++ Assets/Scripts/MoveCamera.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+private var rotation : Vector3;
+private var time : float;
+
+function Start () {
+    time = 0;
+    rotation = transform.rotation.eulerAngles;
+}
+
+function Update () {
+    time += Time.deltaTime;
+    if (time >= 360) time -= 360;
+    var curRotation = rotation + Vector3.up*3*Mathf.Sin(time);
+    transform.rotation.eulerAngles = curRotation;
+}
